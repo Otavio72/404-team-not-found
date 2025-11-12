2a751e592545f3babcb4c6dd29045a5b4d8b499a
Otavio72 <otavioribeiro1932004@gmail.com>
2025-11-12 11:42:29 -0300

    bug label background solved

--- ui/welcome.py
+++ ui/welcome.py
@@ -26,6 +26,10 @@ class WelcomeScreen(ttk.Frame):
 
         style = ttk.Style(theme="flatly")  # tema inicial
 
+        styleLabel = ttk.Style()
+        styleLabel.configure("CardLabel.TLabel", background="#f8f9fa", foreground="#000000")
+        
+
         # ---- DB init --------------------------------------------------------
         self.db = DatabaseManager(db_path)
         self.db.run_schema_file(schema_path)
@@ -44,31 +48,38 @@ class WelcomeScreen(ttk.Frame):
             new_theme = "darkly" if current == "flatly" else "flatly"
             style.theme_use(new_theme)
 
+                # atualizar fundo e texto das labels
+            if new_theme == "flatly":
+                style.configure("CardLabel.TLabel", background="#f8f9fa", foreground="#000000")  # fundo claro, texto escuro
+                card.configure(bootstyle="light")
+            else:
+                style.configure("CardLabel.TLabel", background="#444444", foreground="#ffffff")  # fundo escuro, texto claro
+                card.configure(bootstyle="secondary")
+
                     # --- Botão ---
         toggle_btn = ttk.Button(frameThemeButton, text="Toggle Light/Dark", command=toggle_theme)
         toggle_btn.grid(row=1, column=0, pady=20)
 
 
 
-        card = ttk.Frame(wrapper, padding=30, bootstyle="secondary")
+        card = ttk.Frame(wrapper, padding=30, style="CardLabel.TLabel")
         card.grid(row=1, column=0, sticky=NSEW)
         card.columnconfigure(2, weight=1)
 
-
-
-        ttk.Label(card, text="Welcome!", font=("-size", 14, "-weight", "bold")).grid(
+        
+        ttk.Label(card, text="Welcome!", style="CardLabel.TLabel" , font=("-size", 14, "-weight", "bold")).grid(
             row=0, column=0, pady=(0, 16)
         )
 
         # Name
         self.name_var = ttk.StringVar()
-        ttk.Label(card, text="Name:").grid(row=1, column=0, sticky=W)
+        ttk.Label(card, text="Name:", style="CardLabel.TLabel").grid(row=1, column=0, sticky=W, )
         self.name_entry = ttk.Entry(card, textvariable=self.name_var, width=44)
         self.name_entry.grid(row=2, column=0, sticky=EW, pady=(4, 12))
 
         # Email
         self.email_var = ttk.StringVar()
-        ttk.Label(card, text="Email:").grid(row=3, column=0, sticky=W)
+        ttk.Label(card, text="Email:" , style="CardLabel.TLabel").grid(row=3, column=0, sticky=W)
         self.email_entry = ttk.Entry(
             card, textvariable=self.email_var, width=44)
         self.email_entry.grid(row=4, column=0, sticky=EW, pady=(4, 16))
